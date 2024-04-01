c2a6aa9e01ca88a3489c3ce10ec8b900dd231484
Nagarjuna <nagarjuna993@hotmail.com>
2024-04-02 02:08:25 +0530

    Add project files.

---
+++ StudentAPI/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Name { get; set; } = "";
+
+        public int ContactNumber { get; set; }
+
+        public int Age { get; set; }
+    }
+}
